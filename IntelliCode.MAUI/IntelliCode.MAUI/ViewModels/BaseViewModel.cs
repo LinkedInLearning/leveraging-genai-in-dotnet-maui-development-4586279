aad978b4108df2cd17438182cafbdcae410a3282
Kevin E. Ford <4380198+Bowman74@users.noreply.github.com.>
2024-04-04 15:29:48 -0700

    Start of 02_02

---
+++ IntelliCode.MAUI/IntelliCode.MAUI/ViewModels/BaseViewModel.cs
@@ -0,0 +1,6 @@
+﻿
+namespace IntelliCode.MAUI.ViewModels;
+
+internal class BaseViewModel
+{
+}
---
+++ IntelliCode.MAUI/IntelliCode.MAUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,86 @@
+﻿using IntelliCode.MAUI.Models;
+
+namespace IntelliCode.MAUI.ViewModels;
+internal class MainViewModel : BaseViewModel
+{
+    private Person _Person;
+
+    public MainViewModel()
+    {
+        _Person = new Person();
+    }
+
+    public int Id
+    {
+        get
+        {
+            return _Person.Id;
+        }
+        set
+        {
+            if (_Person.Id != value)
+            {
+                _Person.Id = value;
+            }
+        }
+    }
+
+    public string Name { 
+        get
+        {
+            return _Person.Name;
+        } 
+        set
+        {
+            if (_Person.Name != value) 
+            {
+                _Person.Name = value;
+            }
+        }
+    }
+
+    public string EMail
+    {
+        get
+        {
+            return _Person.Email;
+        }
+        set
+        {
+            if (_Person.Email != value)
+            {
+                _Person.Email = value;
+            }
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            return _Person.Age;
+        }
+        set
+        {
+            if (_Person.Age != value)
+            {
+                _Person.Age = value;
+            }
+        }
+    }
+
+    public DateTime BirthDate
+    {
+        get
+        {
+            return _Person.BirthDate;
+        }
+        set
+        {
+            if (_Person.BirthDate != value)
+            {
+                _Person.BirthDate = value;
+            }
+        }
+    }
+}
