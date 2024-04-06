8a429bf2454b27373c2b4ede406652cecd8d7fb0
Kevin E. Ford <4380198+Bowman74@users.noreply.github.com.>
2024-04-06 11:43:12 -0700

    Start of 02_03

---
+++ IntelliCode.MAUI/IntelliCode.MAUI/Functions/AnotherStringTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliCode.MAUI.Functions
+{
+    internal class AnotherStringTest
+    {
+        internal string Add9(string a9, string b9)
+        { return a9 + b9; }
+        internal string Add10(string a10, string b10)
+        { return a10 + b10; }
+        internal string Add11(string a11, string b11)
+        { return a11 + b11; }
+        internal string Add12(string a12, string b12)
+        { return a12 + b12; }
+        internal string Add13(string a13, string b13)
+        { return a13 + b13; }
+        internal string Add14(string a14, string b14)
+        { return a14 + b14; }
+        internal string Add15(string a15, string b15)
+        { return a15 + b15; }
+    }
+}
