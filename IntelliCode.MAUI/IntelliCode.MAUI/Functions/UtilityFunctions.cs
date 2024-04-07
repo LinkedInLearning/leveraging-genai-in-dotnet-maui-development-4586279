fda63b0f01d44a4ada8b350b91768d2529eb012d
Kevin E. Ford <4380198+Bowman74@users.noreply.github.com.>
2024-04-07 12:40:44 -0700

    End of 02_06

--- IntelliCode.MAUI/IntelliCode.MAUI/Functions/UtilityFunctions.cs
+++ IntelliCode.MAUI/IntelliCode.MAUI/Functions/UtilityFunctions.cs
@@ -5,5 +5,6 @@ internal static class UtilityFunctions
 {
     internal static void RemoveNumberFromList(List<int> searchList, int valueToRemove)
     {
+        searchList.RemoveAll(x => x > valueToRemove);
     }
 }
