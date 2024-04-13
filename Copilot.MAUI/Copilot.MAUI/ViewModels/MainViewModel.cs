2b26bbc1dcd43b62dd04e6ec1d8f899fb333623f
Kevin E. Ford <4380198+Bowman74@users.noreply.github.com.>
2024-04-12 17:51:14 -0700

    End of 03_04

---
+++ Copilot.MAUI/Copilot.MAUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Copilot.MAUI.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _Name;
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                if (_Name == value)
+                    return;
+                _Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private double _Price;
+        public double Price
+        {
+            get => _Price;
+            set
+            {
+                if (_Price == value)
+                    return;
+                _Price = value;
+                OnPropertyChanged(nameof(Price));
+            }
+        }
+    }
+}
