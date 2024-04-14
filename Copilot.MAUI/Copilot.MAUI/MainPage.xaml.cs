1f81591a26a095944cb2621808ce828c490c74d2
Kevin E. Ford <4380198+Bowman74@users.noreply.github.com.>
2024-04-14 10:57:57 -0700

    Start of 03_12

--- Copilot.MAUI/Copilot.MAUI/MainPage.xaml.cs
+++ Copilot.MAUI/Copilot.MAUI/MainPage.xaml.cs
@@ -19,18 +19,6 @@ namespace Copilot.MAUI
 #endif
             base.OnAppearing();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
     }
 
 }
