601787078f09bfc65ac19103f339fbda348f59f8
Kevin E. Ford <4380198+Bowman74@users.noreply.github.com.>
2024-04-25 13:17:43 -0700

    Start of 05_02

---
+++ Calendar.MAUI/Calendar.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Calendar.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
