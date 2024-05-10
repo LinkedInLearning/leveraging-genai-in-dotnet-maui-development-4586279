6753ea3044c6fda747f764b7fdf01ac560b23e61
Kevin E. Ford <4380198+Bowman74@users.noreply.github.com.>
2024-05-10 14:01:42 -0700

    end of 03_11

--- Copilot.MAUI/Copilot.MAUI/StyleSelector.xaml.cs
+++ Copilot.MAUI/Copilot.MAUI/StyleSelector.xaml.cs
@@ -5,18 +5,29 @@ using Microsoft.Maui.Controls;
 
 namespace Copilot.MAUI
 {
+    /// <summary>
+    /// Represents a page that allows the user to select a style.
+    /// </summary>
     public partial class StyleSelector : ContentPage
     {
         private const string lightTheme = "LightTheme";
         private const string darkTheme = "DarkTheme";
         private string currentTheme = "";
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StyleSelector"/> class.
+        /// </summary>
         public StyleSelector()
         {
             InitializeComponent();
             SetCurrentTheme(lightTheme, new LightTheme());
         }
 
+        /// <summary>
+        /// Handles the Clicked event of the Button control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         void Button_Clicked(System.Object sender, System.EventArgs e)
         {
             if (currentTheme == lightTheme)
@@ -30,9 +41,10 @@ namespace Copilot.MAUI
         }
 
         /// <summary>
-        /// Do this to change the current theme
+        /// Sets the current theme.
         /// </summary>
-        /// <param name="themeName"></param>
+        /// <param name="themeName">Name of the theme.</param>
+        /// <param name="theme">The theme.</param>
         void SetCurrentTheme(string themeName, ResourceDictionary theme)
         {
             ICollection<ResourceDictionary> mergedDictionaries = this.Resources.MergedDictionaries;
@@ -45,4 +57,3 @@ namespace Copilot.MAUI
         }
     }
 }
-
